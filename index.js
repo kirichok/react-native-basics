cb7d445288f1c743980d3ec2d33c3a86f4109be7
evgeniy <evgeniy.kirichok@gmail.com>
2018-03-30 19:52:13 +0300

    Adding structure

---
+++ index.js
@@ -0,0 +1,11 @@
+export const test = () => 'Test 111 222 ';
+
+export const hello = () => 'Hello';
+
+'use strict';
+
+const Basics = {
+    get Grid() { return require('components'); }
+};
+
+module.exports = Basics;
