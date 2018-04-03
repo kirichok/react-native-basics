5e50b5fd2b237ff56ac5e152b848c998e9c7504e
evgeniy <evgeniy.kirichok@gmail.com>
2018-04-03 14:43:25 +0300

    Checking using external library

---
+++ defaults/index.js
@@ -0,0 +1,1 @@
+export {default as defFont} from './font';
--- index.js
+++ index.js
@@ -1,11 +1,20 @@
-export const test = () => 'Test 111 222 ';
-
-export const hello = () => 'Hello';
-
 'use strict';
 
+// import {Text} from './components/Custom/Image';
+
+function test() {
+    return 'Test 111 222';
+}
+
 const Basics = {
-    get Grid() { return require('components'); }
+    get test() { return test },
+
+    get Grid() {
+        // import Image from './components/Custom/Image';
+        return require('./components/Custom/Image');
+        //const {Text} = require('./components/Custom/Image');
+        //return Text;
+    }
 };
 
 module.exports = Basics;
