ad90e61574d32595d32754981e142303fa7e7906
evgeniy <evgeniy.kirichok@gmail.com>
2018-04-05 13:40:09 +0300

    - Update link func

--- link.js
+++ link.js
@@ -1,4 +1,5 @@
 module.exports = [{
+    func: require('./src/link'),
     description: 'This action updates your project and links all native dependencies',
     name: 'link react-native-device-info',
 }];
