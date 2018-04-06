a79baddef93f035c28f62cc3152016d1f13f9201
evgeniy <evgeniy.kirichok@gmail.com>
2018-04-06 15:30:47 +0300

    - Create run script

--- link.js
+++
@@ -1,5 +0,0 @@
-module.exports = [{
-    func: require('./src/link'),
-    description: 'This action updates your project and links all native dependencies',
-    name: 'link react-native-device-info',
-}];
